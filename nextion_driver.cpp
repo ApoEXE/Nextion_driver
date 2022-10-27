76886932ea473d7c71fc286e30355b41fb826d32
Javier Pernia <javierpernia89@gmail.com>
2022-10-26 22:10:28 -0500

    added TAG: STARTUP DONE and NEXTION READY to reading thread

--- nextion_driver.cpp
+++ nextion_driver.cpp
@@ -3,20 +3,21 @@
 //
 #include "nextion_driver.h"
 
-char STARTUP[]={0x00,0x00,0x00,0xff,0xff,0xff};
-char NEXTION_READY[]={0x88,0xff,0xff,0xff};
+#define MASK 0x0001
+#define STARTUP_DONE 16777215
+#define NEXTION_RDY 2298478591
 
 struct termios saved_attributes;
 static int fd;
 
-
-void reset_input_mode (void)
+void reset_input_mode(void)
 {
-  tcsetattr (fd, TCSANOW, &saved_attributes);
-  printf("RESET MODE RESTORING");
+    tcsetattr(fd, TCSANOW, &saved_attributes);
+    printf("RESET MODE RESTORING");
 }
 
-Nextion_driver::~Nextion_driver(){
+Nextion_driver::~Nextion_driver()
+{
     close(fd);
 }
 
@@ -24,57 +25,67 @@ void *read_thread(void *arg)
 {
     char buff[BUF_SIZE];
     int _n = 0;
-    memset(buff,0,BUF_SIZE);
-    
+    double tmp = 0;
+    uint16_t flag = 0;
+    memset(buff, 0, BUF_SIZE);
+
     printf("starting reading thread\n");
-        while (1)
+    while (1)
+    {
+        _n = read(fd, (char *)buff, BUF_SIZE);
+        printf("size %d reading= ", _n);
+        if (_n > 3)
         {
-        _n=read(fd,(char*)buff,BUF_SIZE);
-        printf("size %d reading= ",_n);
-        for (size_t i = 0; i < _n; i++)
-        {
-            printf(" %02x ",buff[i]);
-        }
-        printf("\n");
-        for (size_t i = 0; i < _n; i++)
-        {
-            if(buff[i]==STARTUP[i]){
-            if(buff[i+1]==STARTUP[i+1]&&buff[i+2]==STARTUP[i+2]&&buff[i+3]==STARTUP[i+3]&&buff[i+4]==STARTUP[i+4]&&buff[i+5]==STARTUP[i+5])
-                printf("STARTUP \n");
+            for (uint8_t i = 0; i < _n; i++)
+            {
+                printf(" %02x ", buff[i]);
+                if (buff[i] == 0x00)
+                {
+                    tmp = ((uint32_t)buff[i] << 24) | ((uint32_t)buff[i + 1] << 16) | ((uint32_t)buff[i + 2] << 8) | ((uint32_t)buff[i + 3] & 0xff);
+                    if (tmp == STARTUP_DONE)
+                    {
+                        //printf("STARTUP %lf\n", tmp);
+                        flag |= 0x0001;
+                    }
+                }
+                if (buff[i] == 0x88)
+                {
+                    tmp = ((uint32_t)buff[i] << 24) | ((uint32_t)buff[i + 1] << 16) | ((uint32_t)buff[i + 2] << 8) | ((uint32_t)buff[i + 3]);
+                    if (tmp == NEXTION_RDY)
+                    {
+                        //printf("NEXTION READY %lf\n", tmp);
+                        flag |= 0x0002;
+                    }
+                }
             }
-            if(buff[i]==NEXTION_READY[i]){
-                printf("got in\n");
-                printf("%02x, %02x \n",buff[i],NEXTION_READY[i]);
-                printf("%02x, %02x \n",buff[i+1],NEXTION_READY[i+1]);
-                printf("%02x, %02x \n",buff[i+2],NEXTION_READY[i+2]);
-                printf("%02x, %02x \n",buff[i+3],NEXTION_READY[i+3]);
-            if(buff[i+1]==NEXTION_READY[i+1]&&buff[i+2]==NEXTION_READY[i+2]&&buff[i+3]==NEXTION_READY[i+3])
-                printf("NEXTION READY \n");
-            }
-
+            printf("\n");
+            if(flag&MASK)
+            printf("STARTUP \n");
+            if(flag>>1&MASK)
+            printf("NEXTION READY \n");
+            memset(buff, 0, _n);
         }
-        
+    }
 
-        memset(buff,0,_n);
-        }
-        
-        pthread_exit(NULL);
+    pthread_exit(NULL);
 }
 
-Nextion_driver::Nextion_driver(std::string path, int baud) {
-    memset(buff,0,BUF_SIZE);
-    
+Nextion_driver::Nextion_driver(std::string path, int baud)
+{
+    memset(buff, 0, BUF_SIZE);
+
     fd = open(path.c_str(), O_RDWR | O_NOCTTY);
-    
-    if(fd < 0){
+
+    if (fd < 0)
+    {
         printf("Error creating File Descriptor");
         exit(0);
     }
-   
-    printf("PATH: %s BAUD: %d FD: %d \n",path.c_str(),baud,fd);
+
+    printf("PATH: %s BAUD: %d FD: %d \n", path.c_str(), baud, fd);
 
     /* Save the terminal attributes so we can restore them later. */
-    tcgetattr (fd, &saved_attributes);
+    tcgetattr(fd, &saved_attributes);
     atexit(reset_input_mode);
     struct termios tty;
 
@@ -82,67 +93,59 @@ Nextion_driver::Nextion_driver(std::string path, int baud) {
     tty.c_cflag = baudrate(baud) | CRTSCTS | CS8 | CLOCAL | CREAD;
     tty.c_iflag = IGNPAR;
     tty.c_oflag = 0;
-    
+
     /* set input mode (non-canonical, no echo,...) */
     tty.c_lflag = 0;
-         
-    tty.c_cc[VTIME]    = 10;   /* inter-character timer unused */
-    tty.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
-        
+
+    tty.c_cc[VTIME] = 1; /* inter-character timer unused */
+    tty.c_cc[VMIN] = 3;  /* blocking read until 5 chars received */
+
     tcflush(fd, TCIFLUSH);
-    tcsetattr(fd,TCSANOW,&tty);
+    tcsetattr(fd, TCSANOW, &tty);
 
-
-   pthread_t r_thread;
-    int ret =  pthread_create(&r_thread, NULL, &read_thread, NULL);
-    if(ret != 0) {
-            printf("Error: pthread_create() failed\n");
-            exit(EXIT_FAILURE);
+    pthread_t r_thread;
+    int ret = pthread_create(&r_thread, NULL, &read_thread, NULL);
+    if (ret != 0)
+    {
+        printf("Error: pthread_create() failed\n");
+        exit(EXIT_FAILURE);
     }
-
-
 }
 
-
-
-
-void Nextion_driver::write_com(std::string key, int val) {
+void Nextion_driver::write_com(std::string key, int val)
+{
     // write
-    
-    
-    std::string value = key+std::to_string(val);
-    int msg_size=value.length()+3;
+
+    std::string value = key + std::to_string(val);
+    int msg_size = value.length() + 3;
 
     char msg[msg_size];
 
-    strcpy(msg,value.c_str());
+    strcpy(msg, value.c_str());
 
-    msg[msg_size-3]=0xff;
-    msg[msg_size-2]=0xff;
-    msg[msg_size-1]=0xff;
-    
+    msg[msg_size - 3] = 0xff;
+    msg[msg_size - 2] = 0xff;
+    msg[msg_size - 1] = 0xff;
 
-    write(fd, (char*)msg, sizeof(msg));
-   
-    printf("wrote %s \n",msg); 
+    write(fd, (char *)msg, sizeof(msg));
+
+    printf("wrote %s \n", msg);
 }
 
-
-
-
-speed_t Nextion_driver::baudrate(int condition) {
-    speed_t ret=0;
-    switch (condition) {
-        case 9600:
-            ret =  (speed_t) B9600;
-            break;
-        case 4800:
-            ret =  (speed_t) B4800;
-            break;
-        case 38400:
-            ret = (speed_t) B38400;
-            break;
+speed_t Nextion_driver::baudrate(int condition)
+{
+    speed_t ret = 0;
+    switch (condition)
+    {
+    case 9600:
+        ret = (speed_t)B9600;
+        break;
+    case 4800:
+        ret = (speed_t)B4800;
+        break;
+    case 38400:
+        ret = (speed_t)B38400;
+        break;
     }
     return ret;
 }
-
